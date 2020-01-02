48242a2904b538fba3d5cd7c6771c92ea3f7603b
Abhinaba Chowdhury <abhinabachowdhury@1110-IND.local>
2020-01-02 16:37:27 +0530

    sceleton app added.

---
+++ frontends/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Welcome to react starter!!!
+      </header>
+    </div>
+  );
+}
+
+export default App;
